138fd8c4ad41e59a8fca997cf5470c3cfa6aa4a4
AndreBitZ <35458085+AndreBitZ@users.noreply.github.com>
2026-09-04 18:55:39 +0100

    API Mac no renderer

--- macos/preload.cjs
+++ macos/preload.cjs
@@ -2,4 +2,11 @@ const { contextBridge, ipcRenderer } = require("electron");
 contextBridge.exposeInMainWorld("mac", {
   pickFolder: () => ipcRenderer.invoke("pick-folder"),
   defaultFolder: () => ipcRenderer.invoke("default-folder"),
+  lastFolder: () => ipcRenderer.invoke("last-folder"),
+  rememberFolder: (path) => ipcRenderer.invoke("remember-folder", path),
+  revealInFinder: (path) => ipcRenderer.invoke("reveal-in-finder", path),
+  notify: (title, body) => ipcRenderer.invoke("notify", { title, body }),
+  print: () => ipcRenderer.invoke("print"),
+  onOpenFolder: (fn) => ipcRenderer.on("mac:open-folder", (_e, p) => fn(p)),
+  onBackup: (fn) => ipcRenderer.on("mac:backup", () => fn()),
 });
